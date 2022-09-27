bbdb774410fb292d37b2da87e3d83583da34e029
OndrejZetrim <ondra.zema@seznam.cz>
2022-09-27 06:35:40 +0200

    types

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def is_number(text):
+def is_number(text: str) -> bool:
     return text.replace(".", "").isdigit()
 
 
-def get_number(text):
+def get_number(text: str) -> float:
     while True:
         num = input("Zadejte " + text + " cislo: ").replace(",", ".")
         if is_number(num):
@@ -11,16 +11,20 @@ def get_number(text):
             print("Zadejte prosim cislo")
 
 
-def calculate(num1, num2, opr):
+def calculate(num1: float, num2: float, opr: str)-> tuple[int, float]:
+    result: float = None
     match(opr):
         case "+":
-            return num1+num2
+            result = num1+num2
         case "-":
-            return num1-num2
+            result = num1-num2
         case "*":
-            return num1*num2
+            result = num1*num2
         case "/":
-            return num1/num2
+            if num2 == 0:
+                return tuple([-1, None])
+            result = num1/num2
+    return tuple([0, result])
 
 
 def main():
@@ -45,8 +49,12 @@ def main():
                 print("Zadejte prosim operaci z: + - * /")
 
         result = calculate(num1, num2, opr)
-
-        print(num1, opr, num2, " = ", result)
+        if result[0] != 0:
+            print("Nastala chyba")
+            can_continue = "n"
+            continue
+        print(type(result))
+        print(num1, opr, num2, " = ", result[1])
 
         can_continue = input("Chcete pokračovat ve výpočtu Y/N (Y): ").lower()
         
